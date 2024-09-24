fc43da5a9409e1d7067bd7449d06c848c59ca2c1
Mothrax <raulito200418@gmail.com>
2024-09-24 18:20:16 +0200

    Añadida forma de usar un filewriter.read() con un array de String

--- JavaClase/src/accesoADatos/gestionFicheros/lecturaEscritura/Escritor.java
+++ JavaClase/src/accesoADatos/gestionFicheros/lecturaEscritura/Escritor.java
@@ -8,12 +8,68 @@ public class Escritor {
 
             Scanner sc = new Scanner(System.in);
             String texto = sc.nextLine();
-            FileWriter fw = new FileWriter("src\\accesoADatos\\gestionFicheros\\lecturaEscritura\\ficheroPruebaLectura.txt");
+            FileWriter fw = new FileWriter("src\\accesoADatos\\gestionFicheros\\lecturaEscritura\\ficheroPruebaLectura.txt", true);
             for (int j = 0; j <= texto.length() - 1; j++){
                 fw.write((int)texto.charAt(j));
             }
-            fw.close();
             sc.close();
+            String cadena[] = { "Álava",
+                    "Albacete",
+                    "Alicante",
+                    "Almería",
+                    "Asturias",
+                    "Ávila",
+                    "Badajoz",
+                    "Baleares",
+                    "Barcelona",
+                    "Burgos",
+                    "Cáceres",
+                    "Cádiz",
+                    "Cantabria",
+                    "Castellón",
+                    "Ceuta",
+                    "Ciudad Real",
+                    "Córdoba",
+                    "Cuenca",
+                    "Girona",
+                    "Granada",
+                    "Guadalajara",
+                    "Guipúzcoa",
+                    "Huelva",
+                    "Huesca",
+                    "Jaén",
+                    "La Coruña",
+                    "La Rioja",
+                    "Las Palmas",
+                    "León",
+                    "Lleida",
+                    "Lugo",
+                    "Madrid",
+                    "Málaga",
+                    "Melilla",
+                    "Murcia",
+                    "Navarra",
+                    "Ourense",
+                    "Palencia",
+                    "Pontevedra",
+                    "Salamanca",
+                    "Segovia",
+                    "Sevilla",
+                    "Soria",
+                    "Tarragona",
+                    "Tenerife",
+                    "Teruel",
+                    "Toledo",
+                    "Valencia",
+                    "Valladolid",
+                    "Vizcaya",
+                    "Zamora",
+                    "Zaragoza"};
+            for(String s : cadena){
+                fw.write((s + "\n" ));
+            }
+
+            fw.close();
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
