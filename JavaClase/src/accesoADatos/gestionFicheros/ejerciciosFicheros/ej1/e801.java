0223743d6f339cc997aed42df665693c64e1c600
Mothrax <raulito200418@gmail.com>
2024-10-01 17:16:50 +0200

    Ejercicio 1-801 terminado de ficheros de acceso a datos

--- JavaClase/src/accesoADatos/gestionFicheros/ejerciciosFicheros/ej1/e801.java
+++ JavaClase/src/accesoADatos/gestionFicheros/ejerciciosFicheros/ej1/e801.java
@@ -1,5 +1,6 @@
 package accesoADatos.gestionFicheros.ejerciciosFicheros.ej1;
 
+import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.EOFException;
 import java.io.File;
@@ -8,23 +9,77 @@ import java.util.ArrayList;
 
 public class e801 {
     public static void main(String[] args) {
-        File fich = new File("src\\accesoADatos\\gestionFicheros\\ejerciciosFicheros\\ej1\\entrada801.dat");
-        ArrayList <Integer> edades = new ArrayList<>();
-
+        File fich = new File("JavaClase\\src\\accesoADatos\\gestionFicheros\\ejerciciosFicheros\\ej1\\entrada801.dat");
+        ArrayList <Double> edades = new ArrayList<>();
+        ArrayList <Double> desEst = new ArrayList<>();
+        int nAlumnos = 0;
         try {
-            FileInputStream fis = new FileInputStream(fich);
-            DataInputStream dis = new DataInputStream(fis);
-            while (true) {
-                 try {
-                    System.out.println(dis.read());
-    
-                 } catch (EOFException e) {
-                     System.out.println(e.getMessage());
-                 }
-            }
+            DataInputStream dis = new DataInputStream(new BufferedInputStream(new FileInputStream(fich)));
             
+                try {
+                    while (true) {
+                        edades.add((double)dis.readInt());
+                        nAlumnos++;
+                    }
+                    
+                } catch (EOFException e) {
+                    System.out.println("Fin del Archivo");
+                    System.out.println("Número de alumnos: " + nAlumnos);
+                    
+                    // Cálculo de la media de las edades
+                    double mArit = 0;
+                    for (int k = 0; k < edades.size(); k++) {
+                        mArit +=  (double)edades.get(k);
+                    }
+                    mArit = mArit / nAlumnos;
+                    System.out.println("Media aritmética de las edades: " + mArit);
+
+                    // Cálculo de la edad más joven
+                    double mJoven = edades.get(0);
+                    for (int k = 0; k < edades.size(); k++){
+                        if  (edades.get(k) < mJoven) {  
+                            mJoven = edades.get(k);
+                        }
+                    }
+                    System.out.println("Más jóven: " + mJoven);
+
+                    //Cálculo de la edad más viejo
+                    double mViejo = edades.get(0);
+                    for (int k = 0; k < edades.size(); k++) {
+                        if (edades.get(k) > mViejo) {
+                            mViejo = edades.get(k);
+                        }
+                    }
+                    System.out.println("Más viejo: " + mViejo);
+
+                    // Cálculo del número de menores de edad
+                    double menoresEdad = 0;
+                    for(int k = 0; k < edades.size(); k++){
+                        if (edades.get(k) < 18){
+                            menoresEdad++;
+                        }
+                    }
+                    System.out.println("Número de menores de edad: " + menoresEdad);
+
+
+                    //Calculo de la desviación estandar
+                    double mediaDiferencias = 0;
+                    for (int k = 0; k  < edades.size(); k++) {
+                        desEst.add((edades.get(k) - mArit) * edades.get(k));
+                    }
+
+                    for (int k = 0; k < desEst.size(); k++) {
+                        mediaDiferencias += desEst.get(k);
+                    }
+
+                    mediaDiferencias = mediaDiferencias / nAlumnos;
+                    double raiz =  Math.sqrt(mediaDiferencias);
+                    System.out.println("Desviación estándar: " + raiz);
+                }
+
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println("Fin del archivo");
         }
     }
 }
+
