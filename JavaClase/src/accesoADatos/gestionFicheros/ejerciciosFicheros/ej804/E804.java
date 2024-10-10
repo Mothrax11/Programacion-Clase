fa10a1bf59c1627155051e542cd90188bdb473b5
Mothrax <raullg97@educastur.es>
2024-10-10 20:45:32 +0200

    Ejercicio e804 terminado y añadido otro de creacion de archivos xml

--- JavaClase/src/accesoADatos/gestionFicheros/ejerciciosFicheros/ej804/E804.java
+++ JavaClase/src/accesoADatos/gestionFicheros/ejerciciosFicheros/ej804/E804.java
@@ -9,31 +9,35 @@ public class E804 {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        String rutaArchivo = sc.nextLine();
-        String nombreArchivo =  sc.nextLine();
-        File fich = new File (rutaArchivo, nombreArchivo);
+        //String rutaArchivo = sc.nextLine();
+        //String nombreArchivo =  sc.nextLine();
+        File fich = new File ("JavaClase\\src\\accesoADatos\\gestionFicheros\\ejerciciosFicheros\\ej804\\archivos", "e804.txt");
 
         try {
-            int numPalabras = 0;
+            int numPalabras = 1;
             int numLineas = 0;
             int numCaracteres = 0;
             int k;
             FileReader fr = new FileReader(fich);
-            BufferedReader br =new BufferedReader(fr);
+            BufferedReader br =new BufferedReader(new FileReader(fich));
         
-            while((k = br.read()) != -1) {
+            while((k = fr.read()) != -1) {
                     numCaracteres++;
-                    if(k == 32){
+                    if(k == 32  || k == 10) {
                         numPalabras++;
                     }
             }
 
-            while (br.lines() != null){
+            
+            while (br.readLine() != null){
                 numLineas++;
             }
+
+            
+            fr.close();
+            br.close();
             System.out.println("Número de Caracteres: " + numCaracteres);
             System.out.println("Número de Palabras: " + numPalabras);
-
             System.out.println("Número de Líneas: " + numLineas);
 
 
